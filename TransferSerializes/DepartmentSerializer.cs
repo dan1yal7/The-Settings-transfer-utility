2f6d509d378195fe8c1f1edbf0b3ca3ef41df2ac
test <test@gmail.com>
2025-02-22 00:11:51 +0500

    DepartmentSerializer

---
+++ TransferSerializes/DepartmentSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+using DrxTransfer;
+using DrxTransfer.IntegrationServicesClient;
+
+namespace TransferSerializes
+{
+    [Export(typeof(SungeroSerializer))]
+    public class DepartmentSerializer : SungeroSerializer
+    {
+        public DepartmentSerializer()
+        {
+            this.EntityName = "Department";
+            this.EntityType = "IDepartments";
+        }
+        protected override IEnumerable<dynamic> Export()
+        {
+            return IntegrationServiceClient.Instance.For<IDepartments>()
+                .Filter(d => d.Status == "Active")
+                .Expand(d => d.HeadOffice)
+                .Expand(d => d.Manager)
+                .FindEntriesAsync()
+                .Result;
+        }
+    }
+}
