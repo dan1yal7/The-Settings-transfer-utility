cf8515d17d7653aecbb3940ea3ec0ae230395968
test <test@gmail.com>
2025-02-23 21:27:34 +0500

    EmployeeSerializer was added

---
+++ TransferSerializes/EmployeeSerializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+using DrxTransfer;
+using DrxTransfer.IntegrationServicesClient;
+
+namespace TransferSerializes
+{
+    [Export(typeof(SungeroSerializer))]
+    public class EmployeeSerializer : SungeroSerializer
+    {
+        public EmployeeSerializer()
+        {
+            this.EntityName = "Employee";
+            this.EntityType = "IEmployees";
+        }
+        protected override IEnumerable<dynamic> Export()
+        {
+            try
+            {
+                return IntegrationServiceClient.Instance.For<IEmployees>()
+                    .Filter(e => e.Status == "Active")
+                    .FindEntriesAsync()
+                    .Result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"The problem is {ex}");
+            }
+
+            return base.Export();
+        }
+    }
+}
---
+++ TransferSerializes/JobSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+using DrxTransfer;
+using DrxTransfer.IntegrationServicesClient;
+
+namespace TransferSerializes
+{
+    [Export(typeof(SungeroSerializer))]
+    public class JobSerializer : SungeroSerializer
+    {
+        public JobSerializer()
+        {
+            this.EntityName = "Job";
+            this.EntityType = "IJobTitles";
+        }
+
+        protected override IEnumerable<dynamic> Export()
+        {
+            return IntegrationServiceClient.Instance.For<IJobTitles>()
+                .Filter(j => j.Status == "Active")
+                .Expand(j => j.Name)
+                .Expand(j => j.Id)
+                .FindEntriesAsync().Result;
+        }
+    }
+}
