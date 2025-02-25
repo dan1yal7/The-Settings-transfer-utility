97c02cb0e95d228df9c94f8a991040d03938e7b0
test <test@gmail.com>
2025-02-25 10:50:11 +0500

    DocumentGroupSerializer

---
+++ TransferSerializes/DocumentGroupSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+using DrxTransfer;
+using DrxTransfer.IntegrationServicesClient;
+
+namespace TransferSerializes
+{
+    [Export(typeof(SungeroSerializer))]
+    public class DocumentGroupSerializer : SungeroSerializer
+    {
+        public DocumentGroupSerializer()
+        {
+            this.EntityName = "DocumentGroup";
+            this.EntityType = "IDocumentGroupBases";
+        }
+        protected override IEnumerable<dynamic> Export()
+        {
+            return IntegrationServiceClient.Instance.For<IDocumentGroupBases>()
+                .Filter(dg => dg.Status == "Active")
+                .FindEntriesAsync().Result;
+        }
+
+    }
+}
